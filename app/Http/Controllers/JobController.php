30eb0296f91452128ebe1064da631a94021c5fa1
Ahmad Najib <eng.ahmad.najeeb17@gmail.com>
2024-03-16 07:15:21 +0300

    008 Job Page Job Card & Link Button Components

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -34,11 +34,10 @@ class JobController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Job $job)
     {
-        //
+        return view('job.show', compact('job'));
     }
-
     /**
      * Show the form for editing the specified resource.
      */
