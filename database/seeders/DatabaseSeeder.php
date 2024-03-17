5f824b85feb3a193f6d2416cc8f53dc029203e27
Ahmad Najib <eng.ahmad.najeeb17@gmail.com>
2024-03-17 07:22:00 +0300

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory()->create([
-            'name' => 'Ahmad Najeebv',
+            'name' => 'Ahmad Najeeb',
             'email' => 'ahmad@nj.com',
         ]);
         \App\Models\User::factory(300)->create();
