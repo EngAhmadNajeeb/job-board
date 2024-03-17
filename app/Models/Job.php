e7186ed68308e83f58b21558128823c1096aa669
Ahmad Najib <eng.ahmad.najeeb17@gmail.com>
2024-03-17 17:55:58 +0300

    032 Applying for Jobs Did I Already Apply

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Query\Builder as QueryBuilder;
@@ -28,6 +29,15 @@ class Job extends Model
     {
         return $this->hasMany(JobApplication::class);
     }
+    public function hasUserApplied(Authenticatable  |User|int $user): bool
+    {
+        return $this->where('id', $this->id)
+            ->whereHas(
+                'jobApplications',
+                fn($query) => $query->where('user_id', '=', $user->id ?? $user)
+            )->exists();
+    }
+
     public function scopeFilter(Builder|QueryBuilder $query, array $filters): Builder|QueryBuilder
     {
         return $query->when($filters['search'] ?? null, function ($query, $search) {
